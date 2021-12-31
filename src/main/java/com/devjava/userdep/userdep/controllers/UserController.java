ad4d056b8cb1d36ad14940a279dc8909f1710b18
israelisaque <israelisaque@hotmail.com>
2021-12-31 10:33:13 -0300

    Saving a user to the bank

--- src/main/java/com/devjava/userdep/userdep/controllers/UserController.java
+++ src/main/java/com/devjava/userdep/userdep/controllers/UserController.java
@@ -3,10 +3,7 @@ package com.devjava.userdep.userdep.controllers;
 import com.devjava.userdep.userdep.entities.User;
 import com.devjava.userdep.userdep.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +24,11 @@ public class UserController {
     public User findById(@PathVariable Long id) {
         User user = repository.findById(id).get();
         return user;
+    }
+
+    @PostMapping
+    public User insert(@RequestBody User user) {
+        User result = repository.save(user);
+        return result;
     }
 }
